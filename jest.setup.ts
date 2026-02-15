81bd5afb78bb5d44755bb09a4eb28c7dc5cfdfff
Sonu-Moksha <sonu.shettiyar@moksha.ai>
2026-02-15 21:43:25 +0530

    Added: Tasks Ui tests

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,16 +1,23 @@
 import "@testing-library/jest-dom";
-import { TextEncoder, TextDecoder } from 'util';
+import { TextEncoder, TextDecoder } from "util";
+
 (global as any).TextEncoder = TextEncoder;
 (global as any).TextDecoder = TextDecoder;
 
-Object.defineProperty(window, 'matchMedia', {
+global.ResizeObserver = class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+};
+
+Object.defineProperty(window, "matchMedia", {
   writable: true,
   value: jest.fn().mockImplementation((query) => ({
     matches: false,
     media: query,
     onchange: null,
-    addListener: jest.fn(),             
-    removeListener: jest.fn(),          
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
     addEventListener: jest.fn(),
     removeEventListener: jest.fn(),
     dispatchEvent: jest.fn(),
